1e024ccd3db0792098801c7c7b8cb57b20b4fd96
koppu <koppu@DESKTOP-JPAE71K>
2019-08-31 19:49:58 -0400

    First working version of Jedi.

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -6,9 +6,49 @@ public class A1Jedi {
 
 	public static void main(String[] args) {
 		
-		Scanner scan = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 
-		// Your code follows here.
+		int numOfItems = input.nextInt();
+		String[] itemNames = new String[numOfItems];
+		double[] itemPrices = new double[numOfItems];
+		int[] itemQuantity = new int[numOfItems];
+
+		for(int i = 0; i < numOfItems; i++)
+		{
+			itemNames[i] = input.next();
+			itemPrices[i] = input.nextDouble();
+			
+		}	
 		
+		int numCustomer = input.nextInt();
+		String[] fName = new String[numCustomer];
+		String[] lName = new String[numCustomer];
+		int[] numBoughtCustomer = new int[numOfItems];
+		for(int i = 0; i < numCustomer; i++)
+		{
+			fName[i] = input.next();
+			lName[i] = input.next();
+			int numBought = input.nextInt();
+			String[] boughtName = new String[numBought];
+			int[] quantity = new int[numBought];
+			for(int z = 0; z < numBought; z++)
+			{
+				quantity[z] = input.nextInt();
+				boughtName[z] = input.next();
+				
+				for(int y = 0; y < numOfItems; y++)
+				{
+					if(boughtName[z].equals(itemNames[y]))
+					{
+						itemQuantity[y] += quantity[z];
+						numBoughtCustomer[y]++;
+					}
+				}
+			}
+		}
+		for(int i = 0; i < numOfItems; i++)
+		{
+			System.out.println(numBoughtCustomer[i] + " " + "customers bought " + itemQuantity[i] + " " + itemNames[i]);
+		}
 	}
 }
